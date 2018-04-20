77e8d0faa7c8a78f5ba7cace37d775bf130f17d4
danielosborne <nycosborne@gmail.com>
2018-04-19 21:03:41 -0400

    created content-index to customize post on look on main blog page

--- wp-content/themes/one_page/template-parts/content-index.php
+++ wp-content/themes/one_page/template-parts/content-index.php
@@ -18,16 +18,7 @@
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
-
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				one_page_posted_on();
-				one_page_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
+ ?>
 	</header><!-- .entry-header -->
 
 	<?php
