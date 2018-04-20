22866ea544a1134dd2711bbe65566fd3a2ebbb91
danielosborne <nycosborne@gmail.com>
2018-04-20 15:38:48 -0400

    working to post styling

--- wp-content/themes/one_page/template-parts/content-index.php
+++ wp-content/themes/one_page/template-parts/content-index.php
@@ -12,22 +12,26 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>
 	<header class="entry-header">
 		<hr class="artile-line"/>
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
- ?>
+    
+
 	</header><!-- .entry-header -->
-  <div class="blog-post">
 
 
-	<?php
-  the_post_thumbnail('thumbnail');
-	?>
 
-	<div class="entry-content">
+
+
+	<div class="blog-post">
+    <?php
+    the_post_thumbnail('thumbnail');
+    ?>
+    <div class="entry-content">
+    <?php
+    if ( is_singular() ) :
+      the_title( '<h1 class="entry-title">', '</h1>' );
+    else :
+      the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+    endif;
+ ?>
 		<?php
 		the_content( sprintf(
 			wp_kses(
