e1a6a972a9d723c084efc34204acf6596e163407
danielosborne <nycosborne@gmail.com>
2018-04-28 18:02:38 -0400

    Adding new query structure and permalinks

--- wp-content/themes/one_page/front-page.php
+++ wp-content/themes/one_page/front-page.php
@@ -19,16 +19,29 @@ get_header();
 		<main id="main" class="site-main">
 			<div class="landing-page">
 				<h3>Projects :</h3>
+
+
+
+
+
 				<section id="blog" class="page-section">
 							<?php
-							$query = new WP_Query( 'pagename=tech-blog' );
+
+							$query = new WP_Query(array(
+								'post_type' => 'post',
+								'pagename' => 'tech-blog',
+
+							));
+							// $query = new WP_Query( 'pagename=tech-blog' );
 							// The Loop
 							if ( $query->have_posts() ) {
 								while ( $query->have_posts() ) {
 									$query->the_post();
+									?>	<a href="<?php esc_url(the_permalink(150)); ?>"><?php
 									echo '<div class="entry-content">';
 									one_page_post_thumbnail();
 									echo '</div>';
+									?></a><?php
 									break;
 								}
 							}
@@ -57,10 +70,14 @@ get_header();
 				</section>
 				<section id="mobile" class="page-section">
 							<?php
-							$query = new WP_Query( 'pagename=lifting' );
+							// $query = new WP_Query( 'pagename=lifting' );
 							// The Loop
+							$query = new WP_Query(array(
+								'post_type' => 'page',
+								'page_id' => 45,
+
+							));
 							if ( $query->have_posts() ) {
-								echo "string";
 								while ( $query->have_posts() ) {
 									$query->the_post();
 									echo '<div class="entry-content">';
